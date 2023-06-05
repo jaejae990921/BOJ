65f649df101a0b94ef2b17972e405470c804de33
jaejae990921 <jaejae990921@naver.com>
2023-06-05 22:41:33 +0900

    Bronze IV #11365 !밀비 급일

---
+++ Bronze/Bronze IV/11365.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            if (str.equals("END")) {
+                break;
+            }
+            for (int i = str.length() - 1; i >= 0; i--) {
+                bw.write(str.charAt(i));
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
