e705edc70454be68ad4c5567628e04c3f12053a3
jaejae990921 <jaejae990921@naver.com>
2023-07-04 20:47:41 +0900

    Bronze IV #4470 줄번호

---
+++ Bronze/Bronze IV/4470.java
@@ -0,0 +1,20 @@
+    import java.io.BufferedReader;
+    import java.io.BufferedWriter;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.io.OutputStreamWriter;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            int num = Integer.parseInt(br.readLine());
+            String str;
+            for (int i = 0; i < num; i++) {
+                str = br.readLine();
+                bw.write(i + 1 + ". " + str + "\n");
+            }
+            bw.flush();
+            bw.close();
+        }
+    }
