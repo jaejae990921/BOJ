ea8ed84df3cb4f8dc2c9532e5dc2af15e766f1cd
jaejae990921 <jaejae990921@naver.com>
2023-09-22 00:05:31 +0900

    Bronze V #15680 연세대학교

---
+++ Bronze/Bronze V/15680.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+
+		bw.write((n == 0) ? "YONSEI" : "Leading the Way to the Future");
+		bw.flush();
+		bw.close();
+	}
+}
