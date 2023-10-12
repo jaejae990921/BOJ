8a63f41210a11b125372d45f00285386d244a67b
jaejae990921 <jaejae990921@naver.com>
2023-10-12 23:16:59 +0900

    Bronze III #2965 캥거루 세마리

---
+++ Bronze/Bronze III/2965.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int max = Math.max((B - A), (C - B));
+
+		bw.write(max - 1 + "");
+		bw.flush();
+		bw.close();
+	}
+}
