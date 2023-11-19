a74f8198f47202b0907b9c78cf928dd5f135deff
jaejae990921 <jaejae990921@naver.com>
2023-11-19 20:43:52 +0900

    Silver V #11651 좌표 정렬하기 2

---
+++ Silver/Silver V/11651.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][2];
+		
+		StringTokenizer st;
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i][1] + " " + arr[i][0] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
