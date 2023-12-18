8fc9cbc26c0b60f027f33aa36db56c5152b2e0da
jaejae990921 <jaejae990921@naver.com>
2023-12-18 22:11:47 +0900

    Bronze III #1247 부호

---
+++ Bronze/Bronze III/1247.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 3; i++) {
+			BigInteger S = new BigInteger("0");
+			int N = Integer.parseInt(br.readLine());
+			
+			for(int j = 0; j < N; j++) {
+				BigInteger NN = new BigInteger(br.readLine());
+				S = S.add(NN);
+			}
+			
+			if(S.compareTo(BigInteger.ZERO) == -1) {
+				System.out.println("-");
+			}else if(S.compareTo(BigInteger.ZERO) == 1) {
+				System.out.println("+");
+			}else {
+				System.out.println(0);
+			}            
+		}
+	}
+}
