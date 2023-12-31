8cf04202a87220bc03b16607307e5140157f727d
jaejae990921 <jaejae990921@naver.com>
2023-12-31 21:08:03 +0900

    Bronze II #7567 그릇

---
+++ Bronze/Bronze II/7567.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int height = 10;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                height += 5;
+            } else {
+                height += 10;
+            }
+        }
+
+        bw.write(String.valueOf(height));
+        bw.flush();
+        bw.close();
+    }
+}
