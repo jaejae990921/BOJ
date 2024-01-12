3bdd527cab3fc831866d1e7b8c5a5c55170b8a8d
jaejae990921 <jaejae990921@naver.com>
2024-01-12 21:51:29 +0900

    Bronze II #15969 행복

---
+++ Bronze/Bronze II/15969.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        sb.append(nums[N - 1] - nums[0]);
+        System.out.println(sb);
+    }
+}
