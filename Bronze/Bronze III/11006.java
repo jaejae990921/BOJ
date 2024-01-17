2159ad4e2a9b30144551690e86e92f68deb4e57c
jaejae990921 <jaejae990921@naver.com>
2024-01-17 23:10:23 +0900

    Bronze III #11006 남욱이의 닭장

---
+++ Bronze/Bronze III/11006.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st;
+    int N, M;
+
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      st = new StringTokenizer(br.readLine());
+      N = Integer.parseInt(st.nextToken());
+      M = Integer.parseInt(st.nextToken());
+
+      sb.append(((M * 2) - N) + " " + (M - ((M * 2) - N))).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+}
