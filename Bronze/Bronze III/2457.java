b1d2f6e7e5cbea7c24dc8375eaa5f4eca3c539ca
jaejae990921 <jaejae990921@naver.com>
2024-02-04 13:08:28 +0900

    Bronze III #2457 사탕 선생 고창영

---
+++ Bronze/Bronze III/2457.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            br.readLine();
+            int n = Integer.parseInt(br.readLine());
+
+            BigInteger sum = new BigInteger("0");
+
+            for (int j = 0; j < n; j++) {
+                sum = sum.add(new BigInteger(br.readLine()));
+            }
+
+            if (sum.mod(new BigInteger(Integer.toString(n))).equals(BigInteger.ZERO)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
