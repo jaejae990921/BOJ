b50856fef32bc8545d5ca6d7efd41a927d92830c
jaejae990921 <jaejae990921@naver.com>
2024-02-26 20:21:19 +0900

    Bronze III #25630 팰린드롬 소떡소떡

---
+++ Bronze/Bronze III/25630.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int cnt = 0;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                cnt++;
+            }
+        }
+
+        bw.write(cnt + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
