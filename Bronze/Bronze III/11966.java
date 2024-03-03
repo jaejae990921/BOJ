f35b6dc4732e3a79e5b959d832dada784a530871
jaejae990921 <jaejae990921@naver.com>
2024-03-03 14:02:52 +0900

    Bronze III #11966 2의 제곱인가?

---
+++ Bronze/Bronze III/11966.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if ((N & (N - 1)) == 0) {
+            bw.write("1\n");
+        } else {
+            bw.write("0\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
