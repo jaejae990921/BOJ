89302fa7c25e90e4183c8c3fdb138a8c1243868a
jaejae990921 <jaejae990921@naver.com>
2024-04-08 20:12:16 +0900

    Bronze I #11005 진법 변환 2

---
+++ Bronze/Bronze I/11005.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(Integer.toString(N, B).toUpperCase());
+        System.out.println(sb);
+    }
+}
