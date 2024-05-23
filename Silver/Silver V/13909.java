21005c1979fdbaa69290f572842436b35e24d8da
jaejae990921 <jaejae990921@naver.com>
2024-05-23 16:06:06 +0900

    Silver V #13909 창문 닫기

---
+++ Silver/Silver V/13909.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int) Math.sqrt(N));
+        System.out.println(sb);
+    }
+}
