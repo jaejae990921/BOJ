d381487b3d4eb78cb28a533059b9d6d8e62a5e4f
jaejae990921 <jaejae990921@naver.com>
2024-06-09 21:35:57 +0900

    Silver V #16395 파스칼의 삼각형

---
+++ Silver/Silver V/16395.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        sb.append(dp[n][k]);
+        System.out.println(sb);
+    }
+}
