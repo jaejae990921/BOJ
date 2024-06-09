a4c06abcf00ea73883aee53d186cb0d7530fc002
jaejae990921 <jaejae990921@naver.com>
2024-06-10 06:50:48 +0900

    Bronze IV #25238 가희와 방어율 무시

---
+++ Bronze/Bronze IV/25238.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        double damage = a - a * b / 100.0;
+
+        sb.append(damage >= 100 ? 0 : 1);
+        System.out.println(sb);
+    }
+}
