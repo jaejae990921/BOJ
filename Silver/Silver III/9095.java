0ece32d1936838cb18f45024a1f39f17a79e4b45
jaejae990921 <jaejae990921@naver.com>
2024-06-30 21:15:09 +0900

    Silver III #9095 1, 2, 3 더하기

---
+++ Silver/Silver III/9095.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int num;
+
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            num = Integer.parseInt(br.readLine());
+            sb.append(dp[num]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
