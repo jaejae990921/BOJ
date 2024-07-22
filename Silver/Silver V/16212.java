8ba15d6befcf2cc417e8bbe559ab0507cce91fe7
jaejae990921 <jaejae990921@naver.com>
2024-07-22 21:33:59 +0900

    Silver V #16212 정열적인 정렬

---
+++ Silver/Silver V/16212.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int num : arr) {
+            sb.append(num).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
