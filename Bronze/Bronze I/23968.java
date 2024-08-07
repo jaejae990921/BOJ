e1c94af1dd9ca8687ea0ca2cb340fff7da770d54
jaejae990921 <jaejae990921@naver.com>
2024-08-07 22:35:47 +0900

    Bronze I #23968 알고리즘 수업 - 버블 정렬 1

---
+++ Bronze/Bronze I/23968.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int temp, cnt = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    cnt++;
+
+                    if (cnt == K) {
+                        sb.append(arr[j] + " " + arr[j + 1]);
+                        break;
+                    }
+                }
+            }
+
+            if (cnt == K) {
+                break;
+            }
+        }
+
+        if (cnt < K) {
+            sb.append(-1);
+        }
+
+        System.out.println(sb);
+    }
+}
