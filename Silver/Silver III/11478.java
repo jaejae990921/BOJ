cf0d7ab9c680ab6913299337f551bb01588518f4
jaejae990921 <jaejae990921@naver.com>
2024-09-06 19:58:40 +0900

    Silver III #11478 서로 다른 부분 문자열의 개수

---
+++ Silver/Silver III/11478.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        HashSet<String> set = new HashSet<String>();
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+
+        sb.append(set.size());
+        System.out.println(sb);
+    }
+}
