bf2eb4f5c24aa0e7bc4963e55bf36d063843537c
jaejae990921 <jaejae990921@naver.com>
2024-09-30 21:51:07 +0900

    Silver III #20920 영단어 암기는 괴로워

---
+++ Silver/Silver III/20920.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> map = new HashMap<>();
+        String word;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            word = br.readLine();
+
+            if (word.length() < M) {
+                continue;
+            }
+
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+
+        List<String> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (Integer.compare(map.get(o1), map.get(o2)) != 0) {
+                    return Integer.compare(map.get(o2), map.get(o1));
+                }
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                return o1.compareTo(o2);
+            }
+        });
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
