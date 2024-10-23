62a123e6c580b13d686a9dc5672547c295af3916
jaejae990921 <jaejae990921@naver.com>
2024-10-23 23:24:39 +0900

    Bronze I #30455 이제는 더 이상 물러날 곳이 없다

---
+++ Bronze/Bronze I/30455.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append(N % 2 == 0 ? "Duck" : "Goose");
+        System.out.println(sb);
+    }
+}
