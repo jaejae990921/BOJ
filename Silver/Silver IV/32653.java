583288474ef772ac32af00a77ff5fbe11d30d33d
jaejae990921 <jaejae990921@naver.com>
2024-11-27 23:37:18 +0900

    Silver IV #32653 흑백 요리사

---
+++ Silver/Silver IV/32653.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        long answer;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) * 2;
+        }
+
+        answer = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+
+        sb.append(answer);
+        System.out.println(sb);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
