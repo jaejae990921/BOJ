795f390d584f8885960aa7b6785d52da5723d2c1
jaejae990921 <jaejae990921@naver.com>
2025-02-23 19:40:45 +0900

    Bronze V #26082 WARBOY

---
+++ Bronze/Bronze V/26082.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int rival = B / A;
+        int answer = 3 * rival * C;
+
+        sb.append(answer);
+        System.out.println(sb);
+    }
+}
