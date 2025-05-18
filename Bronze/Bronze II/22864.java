a29324bf2766b4891adbe39dfb7c1bf8ba24ae04
jaejae990921 <jaejae990921@naver.com>
2025-05-18 17:45:54 +0900

    Bronze II #22864 피로도

---
+++ Bronze/Bronze II/22864.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int tired = 0;
+        int answer = 0;
+
+        for (int i = 0; i < 24; i++) {
+            if (tired + A > M) {
+                tired -= C;
+                if (tired < 0) {
+                    tired = 0;
+                }
+            } else {
+                answer += B;
+                tired += A;
+            }
+        }
+
+        sb.append(answer);
+        System.out.print(sb);
+    }
+}
