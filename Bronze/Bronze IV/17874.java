ece48b464cce4732fae54265e38330116064af8d
jaejae990921 <jaejae990921@naver.com>
2025-06-20 22:11:24 +0900

    Bronze IV #17874 Piece of Cake!

---
+++ Bronze/Bronze IV/17874.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int maxHeight = Math.max(h, n - h);
+        int maxWidth = Math.max(v, n - v);
+
+        sb.append(maxHeight * maxWidth * 4);
+        System.out.println(sb);
+    }
+}
