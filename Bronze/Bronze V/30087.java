8124552e2f9e139c4f3a4152ff6584612f4f8ab1
jaejae990921 <jaejae990921@naver.com>
2025-06-23 22:40:09 +0900

    Bronze V #30087 진흥원 세미나

---
+++ Bronze/Bronze V/30087.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, String> map = new HashMap<>();
+        String input = "";
+
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            input = br.readLine();
+
+            sb.append(map.get(input)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
