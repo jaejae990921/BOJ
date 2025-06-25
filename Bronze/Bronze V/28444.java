68fa7faf949370f6419f30305a54ec01bc4c83fc
jaejae990921 <jaejae990921@naver.com>
2025-06-25 23:19:09 +0900

    Bronze V #28444 HI-ARC=?

---
+++ Bronze/Bronze V/28444.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        sb.append((H * I) - (A * R * C));
+        System.out.println(sb);
+    }
+}
