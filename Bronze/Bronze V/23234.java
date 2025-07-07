37276db7f94a9bb0b8144277c5238200a4c51b52
jaejae990921 <jaejae990921@naver.com>
2025-07-07 22:50:16 +0900

    Bronze V #23234 The World Responds

---
+++ Bronze/Bronze V/23234.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("The world says hello!");
+        System.out.println(sb);
+    }
+}
