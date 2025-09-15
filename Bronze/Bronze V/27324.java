63d794f72e19d0bf653cc0b489a184a523084ffb
jaejae990921 <jaejae990921@naver.com>
2025-09-15 21:28:07 +0900

    Bronze V #27324 ゾロ目 (Same Numbers)

---
+++ Bronze/Bronze V/27324.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String N = br.readLine();
+
+        if (N.charAt(0) == N.charAt(1)) {
+            sb.append("1");
+        } else {
+            sb.append("0");
+        }
+
+        System.out.println(sb);
+    }
+}
