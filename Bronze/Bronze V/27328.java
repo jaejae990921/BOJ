22c0d614814a39ae4b4e6b5b7bb231eb9d8029d1
jaejae990921 <jaejae990921@naver.com>
2025-09-17 23:05:57 +0900

    Bronze V #27328 三方比較 (Three-Way Comparison)

---
+++ Bronze/Bronze V/27328.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        if (A < B) {
+            sb.append("-1");
+        } else if (A > B) {
+            sb.append("1");
+        } else {
+            sb.append("0");
+        }
+
+        System.out.println(sb);
+    }
+}
