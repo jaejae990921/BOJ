e037819c5dbb9063cc898fc038367e001e722d7d
jaejae990921 <jaejae990921@naver.com>
2025-09-23 20:56:21 +0900

    Bronze III #4740 거울, 오! 거울

---
+++ Bronze/Bronze III/4740.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input = "";
+
+        while ((input = br.readLine()) != null) {
+            if (input.equals("***")) {
+                break;
+            }
+
+            sb.append(new StringBuilder(input).reverse()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
