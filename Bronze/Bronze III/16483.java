517330db26767ab8b7d97b6d107de5dce8a33d71
jaejae990921 <101171774+jaejae990921@users.noreply.github.com>
2025-09-30 21:24:18 +0900

    Bronze III #16438 접시 안의 원

---
+++ Bronze/Bronze III/16483.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        double T = Double.parseDouble(br.readLine());
+
+        sb.append(Math.round(T * T / 4));
+        System.out.println(sb);
+    }
+}
