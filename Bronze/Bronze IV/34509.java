a433ed05f15cf2ffa0f7a17754d35e60cecbd884
jaejae990921 <jaejae990921@naver.com>
2026-01-10 19:14:02 +0900

    Bronze IV #34509 2, 4, 6, 8

---
+++ Bronze/Bronze IV/34509.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(42);
+        System.out.println(sb);
+    }
+}
