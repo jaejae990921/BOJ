fee6e42dfd78ac266ed4cfb279d57b7ab8da1a9b
jaejae990921 <jaejae990921@naver.com>
2026-01-12 19:55:37 +0900

    Bronze IV #15474 鉛筆 (Pencils)

---
+++ Bronze/Bronze IV/15474.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int aPrice = ((N + A - 1) / A) * B;
+        int cPrice = ((N + C - 1) / C) * D;
+
+        sb.append(Math.min(aPrice, cPrice));
+        System.out.println(sb);
+    }
+}
